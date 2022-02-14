73c93c5a0d6e6f9bb8680e60a00e14b088977509
BASSAM <sold@outlook.com>
2022-02-15 02:41:44 +0300

    .gitignore

--- src/main/java/sa/hafralbatin/covid19/repository/emailDAO.java
+++ src/main/java/sa/hafralbatin/covid19/repository/emailDAO.java
@@ -1,7 +1,8 @@
 package sa.hafralbatin.covid19.doa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sa.hafralbatin.covid19.model.email;
-
+@Repository
 public interface emailDAO extends JpaRepository<email,Integer> {
 }
